e009ae9e5fb52705f1c0b1714f504b5113e38f8d
moritaporretti <moritaporretti@ufl.edu>
2025-04-15 00:15:00 -0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -6,7 +6,7 @@ import math
 
 def square_root(a):
     if a < 0:
-        raise ValueError("Cannot take square root of a negative number.")
+        raise ValueError("Cannot take square root of a negative number")
     return math.sqrt(a)
 
 def hypotenuse(a, b):
@@ -18,19 +18,20 @@ def add(a, b):
 def subtract(a, b):
     return a - b
 
-def mul(a, b):
+def multiply(a, b):
     return a * b
 
-def div(a, b):
-    if b == 0:
-        raise ValueError("Cannot divide by zero.")
-    return a / b
+def divide(a, b):
+    if a == 0:
+        raise ZeroDivisionError("Cannot divide by zero")
+    return b / a
 
 def logarithm(a, b):
     if a <= 0 or a == 1 or b <= 0:
-        raise ValueError("Invalid input for logarithm.")
+        raise ValueError("Invalid input for logarithm: base must be > 0 and != 1, and b must be > 0")
     return math.log(b, a)
 
-def exp(a, b):
+def exponent(a, b):
     return a ** b
+
 
