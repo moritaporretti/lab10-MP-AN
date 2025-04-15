946dbf6077237db87f9ac114197919327264c51c
moritaporretti <moritaporretti@ufl.edu>
2025-04-15 00:18:51 -0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -18,20 +18,21 @@ def add(a, b):
 def subtract(a, b):
     return a - b
 
-def multiply(a, b):
+def mul(a, b):
     return a * b
 
-def divide(a, b):
+def div(a, b):
     if a == 0:
         raise ZeroDivisionError("Cannot divide by zero")
     return b / a
 
-def logarithm(a, b):
+def log(a, b):
     if a <= 0 or a == 1 or b <= 0:
-        raise ValueError("Invalid input for logarithm: base must be > 0 and != 1, and b must be > 0")
+        raise ValueError("Invalid input for logarithm")
     return math.log(b, a)
 
-def exponent(a, b):
+def exp(a, b):
     return a ** b
+
 
 
